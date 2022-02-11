52fcb289781582112d5614bbfba8cd75af680829
ryzhovva <ryzhovva@lad24.ru>
2022-02-12 00:43:30 +0300

    fix: types and error handling

--- src/dbms/createOne.ts
+++ src/dbms/createOne.ts
@@ -4,15 +4,18 @@ import { UPLOADS_DIR_NAME, STORAGE_FILE_NAME } from '../../config.json';
 const storagePath = `${UPLOADS_DIR_NAME}/${STORAGE_FILE_NAME}`;
 export const createOne = async (key: number, value: string) => {
   try {
-    let buffer: Buffer;
+    let buffer: Buffer | undefined = undefined;
     try {
       buffer = await fs.readFile(storagePath);
     } catch (error: any) {
-      if ((error.errno = '-2')) {
-        fs.writeFile(storagePath, '', { flag: 'w' });
+      if (error.errno === '-2') {
+        await fs.writeFile(storagePath, '', { flag: 'w' });
+      } else {
+        throw error;
       }
-      throw error;
     }
+
+    if (!buffer) throw Error('Buffer not found');
     console.log('buffer.length', buffer.length);
     const data = buffer.toString();
     const parsedData = data.split('\n');
